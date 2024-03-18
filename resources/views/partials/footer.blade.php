800ae9e5ef3bc325caf36704de8e6b8d999205f7
lorenzodisanto <lore.disanto95@gmail.com>
2024-03-18 15:42:29 +0100

    feat: add footer content

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,92 @@
 <footer>
-    FOOTER
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <div class="row">
+                        <div class="col-3">
+                          <h5>DC COMICS</h5>
+                          <ul>
+                             <li><a href="#">Characters</a></li>
+                             <li><a href="#">Comics</a></li>
+                             <li><a href="#">Movies</a></li>
+                             <li><a href="#">TV</a></li>
+                             <li><a href="#">Games</a></li>
+                             <li><a href="#">Videos</a></li>
+                             <li><a href="#">News</a></li>
+                          </ul>
+
+                          <H5>SHOP</H5>
+                          <ul>
+                             <li><a href="#">Shop DC</a></li>
+                             <li><a href="#">Shop DC Collectibles</a></li>
+                          </ul>
+                        </div>
+                        <div class="col-3">
+                        <h5>DC</h5>
+                   <ul>
+                   <li><a href="#">Terms Of Use</a></li>
+                   <li><a href="#">Privacy policy (New)</a></li>
+                   <li><a href="#">Ad Choices</a></li>
+                   <li><a href="#">Advertising</a></li>
+                   <li><a href="#">Jobs</a></li>
+                   <li><a href="#">Subscriptions</a></li>
+                   <li><a href="#">Talent Workshops</a></li>
+                   <li><a href="#">CPSC Certificates</a></li>
+                   <li><a href="#">Ratings</a></li>
+                   <li><a href="#">Shop Help</a></li>
+                   <li><a href="#">Contact Us</a></li>
+                   </ul>
+                        </div>
+                        <div class="col-3">
+                        <h5>SITES</h5>
+                   <ul>
+                   <li><a href="#">DC</a></li>
+                   <li><a href="#">MAD Magazine</a></li>
+                   <li><a href="#">DC Kids</a></li>
+                   <li><a href="#">DC Universe</a></li>
+                   <li><a href="#">DC Power Visa</a></li>
+                   </ul>
+                        </div>
+                        <div class="col-12">
+                     <p>All Site Content TM and c 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved. <a href="#">Cookies Settings</a></p>
+
+                        </div>
+                    </div>
+                </div>
+
+                <!-- immagine DC -->
+                <div class="col-6">
+                    <div class="footer-image">
+                      <img src="{{ Vite::asset('/resources/images/dc-logo-bg.png')}}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>                                 
+    </div>
+    <div class="footer-bottom">
+        <div class="container d-flex justify-content-between">
+            <div class="button my-4">
+                <h5>SIGN-UP NOW!</h5>
+            </div>
+            <div class="social-container d-flex justify-content-between my-4">
+            <h5>FOLLOW US</h5>
+            <div class="social px-2">
+                <img src="{{ Vite::asset('/resources/images/footer-facebook.png')}}" alt="">
+            </div>
+            <div class="social px-2">
+                <img src="{{ Vite::asset('/resources/images/footer-twitter.png')}}" alt="">
+            </div>
+            <div class="social px-2">
+                <img src="{{ Vite::asset('/resources/images/footer-youtube.png')}}" alt="">
+            </div>
+            <div class="social px-2">
+                <img src="{{ Vite::asset('/resources/images/footer-pinterest.png')}}" alt="">
+            </div>
+            <div class="social px-2">
+                <img src="{{ Vite::asset('/resources/images/footer-periscope.png')}}" alt="">
+            </div>
+            </div>
+        </div>
+    </div>
 </footer>
